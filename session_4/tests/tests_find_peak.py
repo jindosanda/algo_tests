a25d69a8bbb14e288eeba25172272a890379b532
Fabio Di Lauro <fabio.dilauro@gmail.com>
2024-03-13 16:12:11 +0100

    session 4

--- session_4/tests/tests_find_peak.py
+++ session_4/tests/tests_find_peak.py
@@ -36,22 +36,6 @@ class TestExercises(unittest.TestCase):
         self.assertEqual(find_peak([9,9,9,9]),9)
         self.assertEqual(find_peak([10,10,10,10]),10)
 
-    def tests_find_peak_negative(self):
-        self.assertEqual(find_peak([-1,-3,-20,-4,-1,0]),0)
-        self.assertEqual(find_peak([-1,-3,-20,-4,-1,0,-1]),0)
-        self.assertEqual(find_peak([-1,-2,-3]),-1)
-        self.assertEqual(find_peak([-1,-2,-7,-8,-9]),-1)
-        self.assertEqual(find_peak([-9,-6,-5]),-5)
-        self.assertEqual(find_peak([-1,-2,-1,-3]),-1)
-        self.assertEqual(find_peak([-1,-2,-1,0,-3]),0)
-        self.assertEqual(find_peak([-1,-2,-3,-2,-1]),-1)
-        self.assertEqual(find_peak([-1,-2,-3,-4]),-1)
-        self.assertEqual(find_peak([-1,-2]),-1)
-        self.assertEqual(find_peak([-4,-7,-4]),-4)
-        self.assertEqual(find_peak([-7]),-7)
-        self.assertEqual(find_peak([-7,-4]),-4)
-        self.assertEqual(find_peak([-4,-7]),-4)
-
         
         
 if __name__ == '__main__':
